bfc439c9bb4b83c3770735d8c07acc777f51dd70
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-04-17 10:45:57 +0100

    first commit

---
+++ schema.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE school ;
+USE school ;
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    label VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL, 
+    password VARCHAR(255) NOT NULL, 
+    role_id INT NOT NULL,
+   FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE RESTRICT
+);
+
+
+CREATE TABLE classes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    classroom_number INT NOT NULL
+);
+
+
+CREATE TABLE courses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT,
+    total_hours INT NOT NULL,
+    professor_id INT NOT NULL,
+    FOREIGN KEY (professor_id) REFERENCES users(id) ON DELETE RESTRICT
+);
+
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date_of_birth DATE NOT NULL,
+    student_number VARCHAR(50) UNIQUE NOT NULL,
+    user_id INT UNIQUE NOT NULL, 
+    class_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE RESTRICT
+);
+
+
+CREATE TABLE enrollments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    course_id INT NOT NULL,
+    enrolled_at DATE NOT NULL,
+    status ENUM('Actif', 'Terminé') DEFAULT 'Actif',
+    FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE,
+    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE,
+    UNIQUE(student_id, course_id)
+);
